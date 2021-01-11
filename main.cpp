0b0f95174f5ca29a8ff2794b3fe4d113993f35d0
fei-shi <77260424+fei-shi@users.noreply.github.com>
2021-01-11 21:48:20 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "server.h"
+#include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    server w;
+    Widget w;
     w.show();
 
     return a.exec();
